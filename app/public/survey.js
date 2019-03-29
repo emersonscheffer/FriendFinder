b13055fd3680e333fd08b4279964d8b3fcb90f3c
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-03-29 12:23:44 -0700

    updated get values

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,6 +1,26 @@
 //
+var questionCollection = [
+    "Your mind is always buzzing with unexplored ideas and plans.",
+    "Generally speaking, you rely more on your experience than your imagination.",
+    "You find it easy to stay relaxed and focused even when there is some pressure.",
+    "You rarely do something just out of sheer curiosity.",
+    "People can rarely upset you.",
+    "It is often difficult for you to relate to other people’s feelings.",
+    "In a discussion, truth should be more important than people’s sensitivities.",
+    "You rarely get carried away by fantasies and ideas.",
+    "You think that everyone’s views should be respected regardless of whether they are supported by facts or not.",
+    "You feel more energetic after spending time with a group of people."
+];
+
+var question;
 
 function selectFn(n) {
+
+    var questW = document.createElement('h3');
+    questW.innerHTML = "Question " + n;
+    var questP = document.createElement('p');
+    questP.innerHTML = questionCollection[n - 1];
+
     var select = document.createElement('select');
     select.setAttribute('id', "select" + n);
     select.appendChild(document.createElement('option'));
@@ -12,6 +32,9 @@ function selectFn(n) {
         select.lastElementChild.setAttribute("value", i + 1);
         select.lastElementChild.innerHTML = i + 1;
     }
+
+    document.getElementById('questions').appendChild(questW);
+    document.getElementById('questions').appendChild(questP);
     document.getElementById('questions').appendChild(select);
 }
 
@@ -21,7 +44,7 @@ for (let index = 0; index < 10; index++) {
 
 
 
-
+let results = [];
 
 function collect() {
 
@@ -47,6 +70,28 @@ function collect() {
     var val9 = sel9.options[sel9.selectedIndex].value;
     var val10 = sel10.options[sel10.selectedIndex].value;
 
-    console.log(val1);
-    
+
+    results = [];
+    results.push(document.getElementById('name').value);
+    results.push(document.getElementById('link').value);    
+
+    results.push(val1);
+    results.push(val2);
+    results.push(val3);
+    results.push(val4);
+    results.push(val5);
+    results.push(val6);
+    results.push(val7);
+    results.push(val8);
+    results.push(val9);
+    results.push(val10);
+
+    callLog();
+
+    return results;
+}
+
+
+function callLog() {
+    console.log(results);
 }
